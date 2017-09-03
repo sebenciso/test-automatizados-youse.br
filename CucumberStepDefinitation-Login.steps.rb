a6820cf41c84a23ae6ff085497149c91e41c2728
sebenciso <sarhato@outlook.com>
2017-09-03 13:37:05 -0500

    Update CucumberStepDefinitation-Login.steps.rb

--- CucumberStepDefinitation-Login.steps.rb
+++ CucumberStepDefinitation-Login.steps.rb
@@ -1,9 +1,15 @@
 Feature: Login page of Youse
  To test login page of Youse web site
 
-Scenario: Login with valid userid an password
+Scenario1: Login with valid userid an password
 Guiven I should be at the login page
 And I provide valid userid and password 
 When I click on login button
 Then I should be able to login inside the Youse
-And The title of web page should be Youse Main page
+
+Scenario2: Login with valid userid an password no registry
+Given should be at the login page
+And I provide on valid userid an password no value
+When I click on login button
+Then I should be uneable 
+And The title: "Invalid email or password." of web page should be Youse Main page
